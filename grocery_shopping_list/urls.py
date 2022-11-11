a162c1b47a304b1d32c7d677e0776f78a442da30
Stealthy20 <kjellin.fredrik@live.se>
2022-11-11 18:43:11 +0000

    Add allauth

--- grocery_shopping_list/urls.py
+++ grocery_shopping_list/urls.py
@@ -19,5 +19,7 @@ from shopping_list import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home,name="home")
+    path('', views.home,name="home"),
+    path('accounts/', include('allauth.urls')),
 ]
+
